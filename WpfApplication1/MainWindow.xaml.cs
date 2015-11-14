fe790c6a394a11805d35ab2a621d2dc8f4f95795
Richard Griffiths <Soulfiremage@hotmail.co.uk>
2015-11-14 13:18:41 +0000

    Finally, cracked Async/Await and two styles are demonstrated here for my future reference as this notion is likely to confuse me again!

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -8,92 +8,98 @@ using System.IO;
 using System.Windows.Input;
 using System.Windows.Shapes;
 using System.Windows.Media;
-
+using System.Threading.Tasks;
+using System.Threading;
 namespace SuperHeroSchool
 {
     /// <summary>
     /// Welcome to Superhero school. It's a project created solely to learn how to do stuff. Various basic concepts are tried out
     /// including the latest C#6 syntax. As I practice I'll add more features. Eventually there will be a parallel MVC 5 app.
     /// All this app should do at the moment is try to build a school of superheroes with their grades. A chart should be added soon as well.,
+    /// You'll also find two syntax styles for using simple async to get the data.
     /// </summary>
     public partial class MainWindow : Window
         {
 
         public static RoutedCommand ViewStudent = new RoutedCommand();
         private School _school = new School();
-        private Polyline myPolyline;
+       
 
         public School School { get { return _school; }  }
 
         public MainWindow()
         {
             InitializeComponent();
-           
-            StringBuilder sb = new StringBuilder(); // Not strictly necessary, could just update a string or the textbox directly
-                                                    //for now.
-            sb.Append("This is the class list for this year\n");
-          foreach (StudentClass c in School.SuperHeroSchool)
-                {
-                sb.Append($"Class Name: {c.Name} \nDate:{System.DateTime.Today.ToShortDateString()}\n");
-                foreach (Student student in c.Students)
+            TitleText.AppendText("Welcome To the Super Hero Academy Student Grades Application");
+            getHereosTask(); //gethereosTask2 is equivalent I believe!
+        }
+        /// <summary>
+        /// Async style one: here we hide the need for the async inside lambdas :)
+        /// Notice how the second method, Task<string> is NOT async.  Instead we just make a nice async lambda lambada!
+        /// </summary>
+          void getHereosTask()
+            {
+              Task<string> sb = Task.Run(async () => await getHeroes());
+              Wilma.Text = sb.Result;
+            }
+        
+           Task<string> getHeroes()
+            {
+            return   Task.Run(() =>
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (StudentClass c in School.SuperHeroSchool)
                     {
-                    sb.Append($"\n*** {student.Name}***\n");
-                    foreach(var grade in student.Grades)
+                    sb.Append($"Class Name: {c.Name} \nDate:{DateTime.Today.ToShortDateString()}\n");
+                    foreach (Student student in c.Students)
                         {
-                        sb.Append($"{grade.Key} : {grade.Value} \n");
+                        sb.Append($"\n*** {student.Name}***\n");
+                        foreach (var grade in student.Grades)
+                            {
+                            sb.Append($"{grade.Key} : {grade.Value} \n");
+                            }
                         }
                     }
-                }
-            TitleText.AppendText("Welcome To the Super Hero Academy Student Grades Application");
-
-            Wilma.Text = sb.ToString();
-        }
-
-          
-        private void Drawing_MouseDown(object sender, MouseButtonEventArgs e)
-            {
-            myPolyline = new Polyline();
-                {  
-            
-
-
-            
-                {
-                points.Add(new Point(e.MouseDevice.GetPosition(Drawing).X, e.MouseDevice.GetPosition(Drawing).Y));
-                if (points.Count() > 1000) break;
-                }
-           
-            Drawing.Children.Add(myPolygon);
-            }
-           
-
-        private void Drawing_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-            {
-            Drawing_MouseDown(sender, e);
+                return sb.ToString();
+            });
             }
 
         /// <summary>
-        /// Here I'm reading in a file of superheroes, turn them into good little students and randomly assign them to classes.
-        /// At time of writing the function is doing too much but it's an exercise - refactoring will be another exerise.
+        /// Non lambda version, the methods have to be async
         /// </summary>
+        async void getHereosTask2()
+            {
+            Wilma.Text = await getHeroes2();
+            }
 
 
+        async Task<string> getHeroes2()
+            {
+            return await Task.Run(() =>
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (StudentClass c in School.SuperHeroSchool)
+                    {
+                    sb.Append($"Class Name: {c.Name} \nDate:{DateTime.Today.ToShortDateString()}\n");
+                    foreach (Student student in c.Students)
+                        {
+                        //  Task.Delay(TimeSpan.FromMilliseconds(10)).Wait(); just for testing purposes
+                        sb.Append($"\n*** {student.Name}***\n");
+                        foreach (var grade in student.Grades)
+                            {
+                            sb.Append($"{grade.Key} : {grade.Value} \n");
+                            }
+                        }
+                    }
+                return sb.ToString();
+            });
+            }
 
-        //       'Private Sub OnItemSelected(sender As Object, e As RoutedEventArgs) Handles CarePlanTreeView.SelectedItemChanged
-        //'    If IsNothing(CarePlanTreeView.SelectedItem) = False Then
-        //'        Dim context = CarePlanTreeView.SelectedItem
-        //'        Select Case context.[GetType]
-        //'            Case GetType(CarePlan)
-        //'                GetCarePlanContext(context)
-        //'            Case GetType(Field)
-        //'                GetFieldRcpdContext(context)
-        //'            Case GetType(Rcpd)
-        //'                GetRcpdContext(context)
-        //'            Case Else
-        //'                MessageBox.Show(CarePlanTreeView.SelectedItem.ToString())
-        //'        End Select
-        //'    End If
-        //'End Sub
+        //only use for testing
+        private void Drawing_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            {
+            getHereosTask();
+            }
 
         }
  
